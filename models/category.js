aa7ab26797685419fb0da81a13cfd46517aa1e81
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-07-01 16:47:16 -0400

    category model updated

---
+++ models/category.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const categorySchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    password: {
+        type: String,
+       
+    }
+}, {
+    timestamps: true,
+
+})
