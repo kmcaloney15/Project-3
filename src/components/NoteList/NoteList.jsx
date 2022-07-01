13100011fc0149045d70d885a5fb216d94e45afb
earlhalasan <earl.halasan@gmail.com>
2022-07-01 15:22:12 -0400

    Added NoteListItem to NoteList

--- src/components/NoteList/NoteList.jsx
+++ src/components/NoteList/NoteList.jsx
@@ -1,5 +1,10 @@
-export default function NoteList(){
-    return (
-    <h3>Note List</h3>
-    )
+import NoteListItem from "../NoteListItem/NoteListItem";
+
+export default function NoteList() {
+  return (
+    <>
+      <h3>Note List</h3>
+      <NoteListItem />
+    </>
+  );
 }
