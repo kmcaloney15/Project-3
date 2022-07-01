ea820317a92700d012381d996cb3b8762018619c
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-07-01 16:34:46 -0400

    TodoList Form Added

---
+++ src/components/TodoListForm/TodoListForm.jsx
@@ -0,0 +1,38 @@
+
+export default function TodoListForm() {
+    return (
+        <>
+            <h3>Todo List Form</h3>
+            <div>
+                <form action="">
+                    <label >Title</label>
+                    <input type="text" placeholder="write here..." />
+                    <label >Date</label>
+                    <input type="text" placeholder="write here..." />
+
+                    <label >Category</label>
+                    <select name="Category">
+                    <option value="A">a</option>
+                    <option value="B">b</option>
+                    <option value="C">c</option>
+                    </select>
+
+                    <label >Urgency</label>
+                    <select name="urgency">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    </select>
+
+                    <label >Description</label>
+                    <input type="text" placeholder="write here..." />
+                   
+
+                </form>
+            </div>
+
+
+
+        </>
+    )
+}
--- src/pages/TodoIndexPage/TodoIndexPage.jsx
+++ src/pages/TodoIndexPage/TodoIndexPage.jsx
@@ -1,10 +1,13 @@
 import TodoList from "../../components/TodoList/TodoList";
+import TodoListForm from "../../components/TodoListForm/TodoListForm";
 
 export default function TodoIndexPage() {
   return (
     <>
       <h1>TodoIndexPage</h1>
       <TodoList />
+      <TodoListForm />
+
     </>
   );
 }
