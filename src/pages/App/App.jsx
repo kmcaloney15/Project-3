5d748887fbacd3926d861797359d00f59939bdd9
earlhalasan <earl.halasan@gmail.com>
2022-07-03 12:38:32 -0400

    Merge fix hopefully

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -11,7 +11,6 @@ import { getUser } from "../../utilities/users-service";
 export default function App() {
   const [user, setUser] = useState(getUser());
   return (
-<<<<<<< HEAD
     <main className="App flex flex-row">
       {user ? (
         <>
@@ -19,28 +18,15 @@ export default function App() {
           <Routes>
             <Route path="/notes" element={<NoteIndexPage />} />
             <Route path="/todos" element={<TodoIndexPage />} />
-=======
-    <main className="App">
-    {user ? (
-      <>
-        <NavBar user={user} setUser={setUser} />
-        <Routes>
-          <Route path="/notes" element={<NoteIndexPage />} />
-          <Route path="/todos" element={<TodoIndexPage />} />
->>>>>>> main
 
-          <Route path="/" element={<HomePage />} />
-        {/* redirect to Homepage if path in address bar hasn't matched a <Route> above */}
-        {/* <Route path="/*" element={<Navigate to="/" />} /> */}
-        </Routes>
-      </>
-    ) : (
-      <AuthPage setUser={setUser} />
-    )}
-  </main>
-    
-
-
-      
+            <Route path="/" element={<HomePage />} />
+            {/* redirect to Homepage if path in address bar hasn't matched a <Route> above */}
+            {/* <Route path="/*" element={<Navigate to="/" />} /> */}
+          </Routes>
+        </>
+      ) : (
+        <AuthPage setUser={setUser} />
+      )}
+    </main>
   );
 }
