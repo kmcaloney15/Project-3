c36bcc7a9e6fc0fa7d8f0479f03f81de4ab670b8
earlhalasan <earl.halasan@gmail.com>
2022-07-03 12:27:47 -0400

    Adjusted homepage positioning

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -11,7 +11,7 @@ import { getUser } from "../../utilities/users-service";
 export default function App() {
   const [user, setUser] = useState(getUser());
   return (
-    <main className="App">
+    <main className="App flex flex-row">
       {user ? (
         <>
           <NavBar user={user} setUser={setUser} />
