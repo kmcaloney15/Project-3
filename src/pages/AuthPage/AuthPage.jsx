56140eb439b7046be340f6e364a6795bcb67093e
earlhalasan <earl.halasan@gmail.com>
2022-07-05 09:09:01 -0400

    Styling added to Authpage and log in and sign up components

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -17,21 +17,32 @@ export default function AuthPage({ setUser }) {
     //testing so only signup or login show
     <>
       <main className="flex flex-col h-screen">
-        <div className="bg-[#1f1f1f] px-10 flex mt-16" id="diagonal">
-          <div className=" text-[8.5rem] mb-[-29px]" id="revdiagonal">
-            <Logo />
-            {/* <div className="bg-[#7b7e63] px-10"></div>{" "} */}
+        <div
+          className="bg-[#7b7e63] px-24 flex mt-44 mb-[-235px] z-0 "
+          id="diagonal"
+        >
+          <p>&nbsp;</p>
+        </div>
+        <div className="">
+          <div className="bg-[#1f1f1f] px-10 flex mt-10" id="diagonal">
+            <div className="text-[8.5rem] mb-[-29px]" id="revdiagonal">
+              <Logo />
+              {/* <div className="bg-[#7b7e63] px-10"></div>{" "} */}
+            </div>
           </div>
         </div>
-        <div className="p-20">
-          <button onClick={() => setShowLogin(!showLogin)}>
-            {showLogin ? "SIGN UP" : "LOG IN"}
-          </button>
+        <div className="px-20 py-24 relative">
           {showLogin ? (
             <LoginForm setUser={setUser} />
           ) : (
             <SignUpForm setUser={setUser} />
           )}
+          <button
+            onClick={() => setShowLogin(!showLogin)}
+            className="bg-[#1f1f1f] text-white font-medium py-2 px-4 rounded hover:ring hover:ring-orange-400"
+          >
+            {showLogin ? "SIGN UP" : "LOG IN"}
+          </button>
         </div>
       </main>
     </>
