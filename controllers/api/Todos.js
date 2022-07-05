bc26b057dda8ae29bd0cbc04c23ca2ab878a610d
kmcaloney15 <katymcaloney@gmail.com>
2022-07-05 18:59:26 -0400

    added delete, create, show, and find todos to controllers

--- controllers/api/Todos.js
+++ controllers/api/Todos.js
@@ -2,23 +2,37 @@ const Todo = require('../../models/todo');
 
 module.exports = {
   index,
-  create
+  create,
+  findAllTodos,
+  deleteTodo,
+  show
 };
 
 // Sorting all the todos by catetory. Might want to change this later -K
 async function index(req, res) {
-  const todos = await Todo.find({}).sort('name')
+  try {
+  const todos = await Todo.find({})
   // .populate('category').exec();
   // re-sort based upon the sortOrder of the categories
   // todos.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
   res.json(todos);
+  } catch {
+    res.status(400).json('Bad Serverside')
+  }
 }
+
 
 // create new todos
 async function create(req, res) {
-  const todo = new Todo(req.body);
-  await todo.save();
-  res.json(todo);
+  try {
+  const newTodo = await Todo.create(req.body);
+  const todoList = await Todo.find({});
+  todoList.push(newTodo).then((todoList) => {console.log(todoList)})
+  await todoList.save();
+  res.json(todoList);
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 // async function create(req, res) {
@@ -43,4 +57,24 @@ async function create(req, res) {
 // need to find all todos for a specific user
 async function findAllTodos(userId) {
   return await this.find({ user: userId });
+}
+
+// to delete a todo
+async function deleteTodo (req, res) {
+  // console.log(req.body)
+  try {
+    console.log(req.params.id)
+    const one = await Todo.findByIdAndDelete(req.params.id)
+    const todoList = await Todo.find({})
+    await todoList.save()
+    res.json(todoList)
+  } catch (e) {
+    res.status(400).json(e);
+  }
+}
+
+
+async function show(req, res) {
+  const todoList = await Todo.findById(req.params.id);
+  res.json(todoList);
 }
