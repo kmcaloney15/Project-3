fa87991a796100db75226140ee29a435e8587dd6
kmcaloney15 <katymcaloney@gmail.com>
2022-07-05 14:48:58 -0400

    creaded seedTodo file and testing

--- controllers/api/Todos.js
+++ controllers/api/Todos.js
@@ -2,20 +2,39 @@ const Todo = require('../../models/todo');
 
 module.exports = {
   index,
-  show
+  create
 };
 
 // Sorting all the todos by catetory. Might want to change this later -K
 async function index(req, res) {
-  const todos = await Todo.find({}).sort('name').populate('category').exec();
+  const todos = await Todo.find({}).sort('name')
+  // .populate('category').exec();
   // re-sort based upon the sortOrder of the categories
-  todos.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+  // todos.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
   res.json(todos);
 }
 
 // create new todos
-async function createTodo(req, res) {
+async function create(req, res) {
   const todo = new Todo(req.body);
   await todo.save();
   res.json(todo);
 }
+
+// async function create(req, res) {
+//   try {
+//     const user = await User.create(req.body);
+//     // token will be a string
+//     const token = createJWT(user);
+//     // send back the token as a string
+//     // which we need to account for 
+//     // in the client
+//     res.json(token);
+//   } catch (e) {
+//     res.status(400).json(e);
+//   }
+// }
+
+// async function create(req, res) {
+//   try {
+//     const user = await User.create(req.body);
