021722a3db681b57a836b5bb8e6d8497c98ee7d2
earlhalasan <earl.halasan@gmail.com>
2022-07-05 19:56:00 -0400

    Added link for new todo. working on new note item

--- src/components/NoteListItem/NoteListItem.jsx
+++ src/components/NoteListItem/NoteListItem.jsx
@@ -1,7 +1,66 @@
+import * as noteAPI from "../../utilities/notes-api";
+import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+
 export default function NoteListItem() {
+  const [formData, setFormData] = useState({
+    title: "",
+    body: "",
+  });
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    //sending new data to backend
+    const addCat = await noteAPI.newCat(formData);
+    // get data again from the backend
+    const cats = await noteAPI.getAll();
+    // return setAllCats(cats);
+  }
+
+  //*** function = form data ***//
+  function handleChange(evt) {
+    const updatedCat = { [evt.target.name]: evt.target.value };
+    setFormData(updatedCat);
+    console.log(formData);
+    // setNewCat(evt.target.value);
+  }
+
   return (
     <>
       <h5>NoteListItem</h5>
+      <br />
+      <form action="">
+        <input
+          name="title"
+          value={formData.title}
+          type="text"
+          placeholder="Note Title"
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <input
+          name="Category"
+          value={formData.category}
+          type="text"
+          placeholder="Category"
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <input
+          name="Note"
+          value={formData.body}
+          type="text"
+          placeholder="Begin writing note"
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <button type="submit" onClick={handleSubmit}>
+          Add New Note{" "}
+        </button>
+      </form>
     </>
   );
 }
---
+++ src/utilities/notes-api.js
@@ -0,0 +1,35 @@
+//to edit and delete category schema, it seems like we need to build api,routes,controller files for the categories. - Sam
+import { getToken } from "./users-service";
+
+const BASE_URL = "/api/notes";
+
+export function getAll() {
+  return sendRequest(BASE_URL);
+}
+
+export function deleteCat(id) {
+  return sendRequest(`${BASE_URL}/deleteCat/${id}`, "DELETE");
+}
+
+// Add an cat to the cat list
+export function newCat(catData) {
+  // Just send itemId for best security (no pricing)
+  // console.log(catData)
+  return sendRequest(`${BASE_URL}/newCat`, "POST", catData);
+}
+
+/*--- Helper Functions ---*/
+
+async function sendRequest(url, method = "GET", payload = null) {
+  const options = { method };
+  if (payload) {
+    options.headers = { "Content-Type": "application/json" };
+    options.body = JSON.stringify(payload);
+  }
+  const res = await fetch(url, options);
+  // res.ok will be false if the status code set to 4xx in the controller action
+  console.log(options);
+  if (res.ok) return res.json();
+  console.log(res.json);
+  throw new Error("Bad Request");
+}
