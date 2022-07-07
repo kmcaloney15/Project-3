31955cbc186b2d87f051d97dfadd3a43caac82d5
Yul3102 <YOUR-EMAIL-ADDRESS>
2022-07-07 13:36:21 -0400

    category debugging solved

--- src/pages/CategoryIndexPage/CategoryIndexPage.jsx
+++ src/pages/CategoryIndexPage/CategoryIndexPage.jsx
@@ -1,11 +1,11 @@
 import CategoryList from "../../components/CategoryList/CategoryList";
 
-export default function NoteIndexPage({allCats, setAllCats}) {
+export default function NoteIndexPage({allCats, setAllCats,setUpdated}) {
   return (
     <>
       <div>
         <h1>Category List</h1>
-        <CategoryList allCats={allCats} setAllCats={setAllCats} />
+        <CategoryList allCats={allCats} setAllCats={setAllCats} setUpdated={setUpdated} />
       </div>
     </>
   );
