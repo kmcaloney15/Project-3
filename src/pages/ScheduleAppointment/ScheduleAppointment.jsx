48d6f192b2270341fef84f22dd5016d5336c9a1b
kmcaloney15 <katymcaloney@gmail.com>
2022-07-08 16:10:16 -0400

    fixed appointment path error

--- src/pages/ScheduleAppointment/ScheduleAppointment.jsx
+++ src/pages/ScheduleAppointment/ScheduleAppointment.jsx
@@ -6,7 +6,10 @@ import { InlineWidget } from "react-calendly";
 export default function ScheduleAppointment(){
     return(
         <div className="font-extralight text-2xl text-left h-1/2 px-2 py-2 border-[#1f1f1f]">
-        <h1>Schedule Appointment</h1>
+        <h1 className="border-black border-b-[1px] mb-5">Schedule Appointment</h1>
+        <p className="text-xs mb-10"> 
+            Schedule time for your self or create an appointment to remind yourself of important things.
+        </p>
 
          <div className="App px-4">
           <InlineWidget url="https://calendly.com/earl-halasan?background_color=f7f7f2&primary_color=fb923c" />
