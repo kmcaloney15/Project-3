397a1e151dc00a8e844edc33584140c3eaf6cb9f
earlhalasan <earl.halasan@gmail.com>
2022-07-09 23:35:40 -0400

    ignore this push Ill fix in merge conflicts

--- src/components/NoteListForm/NoteListForm.jsx
+++ src/components/NoteListForm/NoteListForm.jsx
@@ -1,15 +1,16 @@
 import * as noteAPI from "../../utilities/notes-api";
 import { Link } from "react-router-dom";
-import { useState} from "react";
+import { useState, useEffect, useRef } from "react";
 
-export default function NoteListForm({ allNotes, setAllNotes, setUpdated, allCats }) {
-  // const [activeNote, setActiveNote] = useState([]);
+export default function NoteListForm({ allNotes, setAllNotes, setUpdated }) {
+  // const [allNotes, setAllNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     category: "",
     body: "",
   });
-  let a = setUpdated()
+  let a = setUpdated;
   // useEffect(function () {
   //   async function getNotes() {
   //     const notes = await noteAPI.getAll();
@@ -36,11 +37,11 @@ export default function NoteListForm({ allNotes, setAllNotes, setUpdated, allCat
   async function handleSubmit(evt) {
     evt.preventDefault();
     console.log(allNotes);
-    // setAllNotes([...allNotes, formData]);
-   noteAPI.newNote(formData);
-   setUpdated(!a);
-   console.log(allNotes)
-  
+    setAllNotes([...allNotes, formData]);
+
+    noteAPI.newNote(formData);
+    setUpdated(!a);
+    console.log(allNotes);
     setFormData({
       title: "",
       category: "",
@@ -73,23 +74,14 @@ export default function NoteListForm({ allNotes, setAllNotes, setUpdated, allCat
           />
           <br />
           <i class="fa-solid fa-tag text-orange-500"></i> &nbsp;
-
-
-
-          <select name="category" value={formData.category }  onChange={handleChange} className="font-extralight text-2l text-left h-1/2 px-2 py-2">
-
-            {allCats.map((cat) => <option value={cat._id} key={cat._id} >{cat.title}</option>)}
-
-          </select>
-
-          {/* <input
+          <input
             name="category"
             value={formData.category}
             type="text"
             placeholder="Category"
             onChange={handleChange}
             className="bg-[#f7f7f2] text-sm outline-0"
-          /> */}
+          />
           <br />
           <br />
           <input
