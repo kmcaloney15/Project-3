9d99e74f3698aa7c97294d6a0f03fe2bf0306ae7
earlhalasan <earl.halasan@gmail.com>
2022-07-10 16:31:58 -0400

    styling added to home page and notes now show horizontally

---
+++ src/components/NoteHorizontal/NoteHorizontal.jsx
@@ -0,0 +1,186 @@
+import NoteListForm from "../NoteListForm/NoteListForm";
+import { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import * as noteAPI from "../../utilities/notes-api";
+import { InlineWidget } from "react-calendly";
+
+export default function NoteList({
+  allNotes,
+  setAllNotes,
+  setUpdated,
+  allCats,
+  activeCat,
+}) {
+  // const [allNotes, setAllNotes] = useState([]);
+  // const [activeNote, setActiveNote] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "",
+    category: "",
+    body: "",
+  });
+
+  // not sure if this is needed anymore
+  //*** function = Getting Data From Backend  ***//
+  // useEffect(function () {
+  //   async function getNotes() {
+  //     const notes = await noteAPI.getAll();
+  //     setAllNotes(notes);
+  //     console.log(allNotes);
+  //   }
+  //   getNotes();
+  // }, []);
+  // console.log(allNotes);
+
+  //*** fucntion = creating new category ***//
+  async function deleteNote(evt) {
+    console.log(evt.target.value);
+    //sending new data to backend
+    // get data again from the backend
+    const notes = allNotes.filter((note) => note._id !== evt.target.value);
+    console.log(notes);
+    setAllNotes(notes);
+    await noteAPI.deleteNote(evt.target.value);
+    // const addNote = await noteAPI.deleteNote(evt.target.value);
+  }
+
+  // *** fucntion = editing a category ***//
+  async function editNote(evt) {
+    console.log(evt.target.value);
+
+    // FrontEnd updating
+    const notes = allNotes.filter((note) => note._id === evt.target.value);
+    // notes[0].title = formData.title;
+    // setEdit(!edit);
+    // console.log(cats[0].title);
+    // console.log(formData)
+
+    //Backend updating
+    noteAPI.editNote(evt.target.value, formData);
+    setFormData({
+      title: "",
+      category: "",
+      body: "",
+    });
+  }
+
+  // //*** fucntion = creating new category ***//
+  // async function handleSubmit(evt) {
+  //   evt.preventDefault();
+  //   //sending new data to backend
+  //   const addNotes = await noteAPI.newNote(formData);
+  //   // get data again from the backend
+  //   const notes = await noteAPI.getAll();
+  //   return setAllNotes(notes);
+  // }
+
+  //*** function = form data ***//
+  function handleChange(evt) {
+    const updatedNote = { ...formData, [evt.target.name]: evt.target.value };
+    setFormData(updatedNote);
+    console.log(formData);
+  }
+
+  //*** function = Edit data ***//
+  function handleEditing(evt) {
+    console.log("edit mode activated");
+    setEdit(!edit);
+  }
+
+  let viewMode = {};
+  let editMode = {};
+
+  if (edit) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+
+  function catFinder(a) {
+    let r = allCats.filter((cat) => cat._id === a);
+    console.log(allNotes);
+    console.log(r);
+    console.log(r[0].title);
+    return r[0].title;
+  }
+  //  let b = allNotes[0].category._id
+  //   console.log(allCats[7]._id);
+  //   console.log(allNotes[0].category._id);
+  //   catFinder(b)
+
+  return (
+    <>
+      <div className="flex">
+        <div className="h-screen ">
+          {allNotes ? (
+            <div className="flex">
+              <div
+                className="text-black flex flex-row-reverse justify-items-start order-last border-[#7b7e63] focus:text-black focus:bg-[#f7f7f2] font-extralight"
+                aria-selected="false"
+              >
+                {allNotes.map((note, idx, { setEdit }) => (
+                  <>
+                    <div
+                      className="border-black border-[1px] rounded-md pt-2 pb-4 px-4 font-light my-3 w-[14rem] text-left mr-5"
+                      id="hardshadow"
+                      key={idx}
+                      // don't have this in todoList
+                      // onClick={() => setActiveNote(note)}
+                    >
+                      {/* <Link to={`/notes/${note._id}`} style={viewMode}> */}
+                      <p className="border-black border-b-[1px] text-lg">
+                        {note.title}
+                      </p>
+                      <i class="fa-solid fa-tag mt-1.5 pl-1.5 text-orange-500"></i>{" "}
+                      &nbsp;{" "}
+                      <a className="text-sm ml-[-3px]">
+                        {catFinder(note.category._id)}
+                      </a>
+                      <p className="pt-2">{note.body}</p>
+                      <button
+                        value={note._id}
+                        className="bg-[#1f1f1f] flex items-end font-light text-sm text-white mt-4 py-1 px-3 rounded-lg hover:ring hover:ring-orange-400"
+                        // style={editMode}
+                        onClick={deleteNote}
+                      >
+                        Delete
+                      </button>
+                      {/* </Link> */}
+                      <input
+                        type="text"
+                        className="textInput"
+                        style={editMode}
+                        placeholder={note.title}
+                        onChange={handleChange}
+                      />
+                      {/* <button
+                        className="border-1 border-black bg-[#7b7e63]  rounded text-white text-sm px-1 mx-2"
+                        onClick={editNote}
+                        type="submit"
+                        style={editMode}
+                      >
+                        Edit
+                      </button> */}
+                      {/* <button
+                        className="border-1 border-black bg-[#7b7e63]  rounded text-white text-sm px-1 mx-2"
+                        type="submit"
+                        value={note._id}
+                        style={editMode}
+                        onClick={handleEditing}
+                      >
+                        Save
+                      </button> */}
+                      {/* <br></br> */}
+                    </div>
+                  </>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <h5>loading</h5>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import * as todoAPI from "../../utilities/todos-api";
 import * as catAPI from "../../utilities/categories-api";
 import * as noteAPI from "../../utilities/notes-api";
 import TodoListItem from "../../components/TodoListItem/TodoListItem";
+import NoteHorizontal from "../../components/NoteHorizontal/NoteHorizontal";
 
 export default function HomePage({
   user,
@@ -23,30 +24,34 @@ export default function HomePage({
 
   return (
     <>
-      <div className="w-screen px-8">
+      <div className="w-screen h-screen px-8 overflow-hidden">
         <h1 className="font-medium text-3xl text-left px-2 pt-6 pb-2 border-[#1f1f1f] border-b-[1px]">
-          <i className="fa-solid fa-house text-orange-400"></i>&nbsp; Hello,{" "}
-          {user.name}!
+          <i className="fa-solid fa-house text-orange-400"></i>
+          &nbsp; Hello, {user.name}!
         </h1>
-        <div className=" h-[85%] ">
-          <div className="font-light text-left h-1/2 px-2 py-2 border-[#1f1f1f] border-b-[1px]">
-            <h1 className="text-2xl font-extralight">Notes</h1>
-            {/* <NoteList
-              allNotes={allNotes}
-              setAllNotes={setAllNotes}
-              setUpdated={setUpdated}
-              allCats={allCats}
-              activeCat={activeCat}
-            /> */}
+        <div className="h-[85%]">
+          <div className="font-light text-left h-1/2 px-2 pt-2 border-[#1f1f1f] border-b-[1px] overflow-hidden">
+            <div>
+              <h1 className="text-2xl font-extralight">Notes</h1>
+            </div>
+            <div className="overflow-auto">
+              <NoteHorizontal
+                allNotes={allNotes}
+                setAllNotes={setAllNotes}
+                setUpdated={setUpdated}
+                allCats={allCats}
+                activeCat={activeCat}
+              />
+            </div>
           </div>
-          <div className="font-extralight text-2xl text-left h-1/2 px-2 py-2">
+          <div className="font-extralight text-2xl text-left h-1/2 px-2 pt-2">
             <h1>To-Do's</h1>
-            <TodoList
+            {/* <TodoList
               allTodos={allTodos}
               setAllTodos={setAllTodos}
               setUpdated={setUpdated}
               activeCat={activeCat}
-            />
+            /> */}
           </div>
         </div>
         {/* <button onClick={handleCheckToken}>Check When My Login Expires</button> */}
